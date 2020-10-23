81de26fb026f8b3ae1f0802f436415c202fbcd71
Vanaid <69782092+Vanaid@users.noreply.github.com>
2020-10-23 10:35:33 -0500

    feat: brackets solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let result = str;
+    let regStr = '';
+
+    bracketsConfig.forEach(e => {
+      let brackets = e[0] + e[1];
+      if (isNaN(brackets)) {
+        regStr += `\\${e[0]}\\${e[1]}|`;
+      } else {
+        regStr += `${e[0]}${e[1]}|`;
+      }
+    });
+
+    regStr = regStr.slice(0, -1);
+
+    let regExpBrackets = new RegExp(regStr, 'g');
+
+    for(let i = 0; i < (str.length / 2); i++) {
+      result = result.replace(regExpBrackets, '');
+    };
+
+    return result.length === 0;
+};
